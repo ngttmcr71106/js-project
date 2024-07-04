de46cfb1ac09a137811c4176a754047e33248a07
noguchi tomoki <noguchi.5tag@gmail.com>
2024-07-04 21:45:00 +0900

    更新

--- design/ローディング/ロゴアニメーション/script.js
+++ design/ローディング/ロゴアニメーション/script.js
@@ -1,5 +1,19 @@
-//logoの表示
+//SVGアニメーションの描画
+var stroke;
+stroke = new Vivus('mask', {//アニメーションをするIDの指定
+    start:'manual',//自動再生をせずスタートをマニュアルに
+    type: 'scenario-sync',// アニメーションのタイプを設定
+    duration: 10,//アニメーションの時間設定。数字が小さくなるほど速い
+    forceRender: false,//パスが更新された場合に再レンダリングさせない
+    animTimingFunction:Vivus.EASE,//動きの加速減速設定
+},
+function(){
+       $("#mask").attr("class", "done");//描画が終わったらdoneというクラスを追加
+}
+);
+
 $(window).on('load',function(){
-$("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-$("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+  $("#splash").delay(3000).fadeOut('slow');//ローディング画面を3秒（3000ms）待機してからフェイドアウト
+	$("#splash_logo").delay(3000).fadeOut('slow');//ロゴを3秒（3000ms）待機してからフェイドアウト
+        stroke.play();//SVGアニメーションの実行
 });
